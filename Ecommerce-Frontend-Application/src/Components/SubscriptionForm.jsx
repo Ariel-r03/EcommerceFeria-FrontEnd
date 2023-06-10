e14407902e0d94f1fedc54c030437db9fb4dbe98
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-06-10 09:10:29 -0600

    Fixing some design details

--- Ecommerce-Frontend-Application/src/Components/SubscriptionForm.jsx
+++ Ecommerce-Frontend-Application/src/Components/SubscriptionForm.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function SubscriptionForm() {
   return (
     <section className='bg-white px-[3rem] py-[2rem] flex flex-col justify-center items-center w-full sm:w-[80%]'>
-      <p className='mt-2 mb-5 font-extrabold text-[30px]'>¡Suscribete para novedades!</p>
+      <p className='mt-2 mb-5 font-extrabold text-[30px]'>¡Suscribete para <span className='flex justify-center sm:inline-block'> novedades!</span></p>
       <span className='text-[16px]'>¡No te pierdas ningún nuevo artículo en la tienda!</span>
-      <div className='mt-[1.5rem] sm:mt-[3rem]'>
-        <input className='border-2 w-[250px] h-[35px] mr-[1rem] mb-[1rem]' placeholder='Email' type="text" />
+      <div className='mt-[1.5rem] sm:mt-[3rem] flex flex-col justify-center sm:inline-block'>
+        <input className='border-2 w-[250px] h-[35px] mr-[1rem] mb-[1rem] px-3' placeholder='Email' type="text" />
         <button className='font-semibold bg-smoothSlate w-[250px] h-[35px]'>Suscribirme</button>
       </div>
     </section>
--- Ecommerce-Frontend-Application/src/Pages/HomePage/HomePageComponents/Cardbenefit.jsx
+++ Ecommerce-Frontend-Application/src/Pages/HomePage/HomePageComponents/Cardbenefit.jsx
@@ -4,8 +4,8 @@ function Cardbenefit({benefit}) {
   return (
     <div className='flex flex-col'>
         <img className='w-[70px] h-[70px] self-center my-2' src={benefit.icon} alt="" />
-        <h1 className='text-[16px] self-center font-extrabold my-2'>{benefit.title}</h1>
-        <p className='text-[14px] '>{benefit.description}</p>
+        <h1 className='text-[13px] sm:text-[16px] self-center font-extrabold my-2'>{benefit.title}</h1>
+        <p className='text-[12px] sm:text-[14px] sm:min-h-[130px] text-justify '>{benefit.description}</p>
         <button className='border-2 border-solid border-slate-950 w-[115px] self-center my-5 '>Leer más</button>
     </div>
   )

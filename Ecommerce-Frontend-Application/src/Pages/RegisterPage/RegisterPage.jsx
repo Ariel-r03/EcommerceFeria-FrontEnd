97143a2e82353b2226a3c5e68da68f24b1aa60a2
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-06-10 18:48:16 -0600

    Init RegisterPage

---
+++ Ecommerce-Frontend-Application/src/Pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavBarComponent} from '../../Components'
+import {RegisterForm} from './RegisterPageComponents'
+function RegisterPage() {
+  return (
+    <div>
+      <div><NavBarComponent/></div>
+      <div><RegisterForm/> </div>
+    </div>
+  )    
+}
+
+export default RegisterPage
---
+++ Ecommerce-Frontend-Application/src/Pages/RegisterPage/RegisterPageComponents/RegisterForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterForm() {
+  return (
+    <section>
+      Hola
+    </section>
+  )
+}
+
+export default RegisterForm

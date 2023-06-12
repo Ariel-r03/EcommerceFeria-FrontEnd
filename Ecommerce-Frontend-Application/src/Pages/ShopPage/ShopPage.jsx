49a182b0dcbb3ca38fe30ea52ef7980bf3c2eb87
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-06-12 11:17:05 -0600

    ShopPage route added

---
+++ Ecommerce-Frontend-Application/src/Pages/ShopPage/ShopPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {NavBarComponent} from '../../Components'
+function ShopPage() {
+  return (
+    <div>
+      <div>
+        <NavBarComponent/>
+      </div>
+    </div>
+  )
+}
+
+export default ShopPage

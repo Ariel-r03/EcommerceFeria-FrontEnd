5122461f8e17ed29cfb48634411fdee18b2e76fa
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-06-21 23:47:31 -0600

    Desktop view almost done

--- Ecommerce-Frontend-Application/src/Pages/ShoppingCartPage/ShoppingCartComponents/ShoppingCartBody.jsx
+++ Ecommerce-Frontend-Application/src/Pages/ShoppingCartPage/ShoppingCartComponents/ShoppingCartBody.jsx
@@ -1,33 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ShoppingCartContext from "../../../Contexts/ShoppingCart/ShoppingCartProvider";
 import { ShoppingCartProductCard } from "../ShoppingCartComponents";
-import { Check, ShopCart,CreditCards } from "../../../Assets";
+import { Check, ShopCart, CreditCards } from "../../../Assets";
+import { useNavigate } from "react-router-dom";
 
 function ShoppingCartBody() {
+  const navigate = useNavigate();
   const { cartProducts } = useContext(ShoppingCartContext);
-
+  const [envio, setEnvio] = useState(100);
+  const subTotal = cartProducts.reduce(
+    (total, obj) => total + parseFloat(obj.price),
+    0
+  );
+  const impuestos = subTotal * 0.15;
+  const total = subTotal + impuestos;
   if (cartProducts.length === 0) {
     return (
-      <h1 className="text-slate-900 text-xl font-bold text-center">
-        Tu carrito de compras esta vacio.
-      </h1>
+      <div className="min-h-screen">
+        <h1 className="text-slate-900 text-xl font-bold text-center">
+          Tu carrito de compras esta vacio.
+        </h1>
+      </div>
     );
   }
 
   return (
-    <section className="">
-      <div className="flex flex-col">
-        <div>
+    <section className="sm:flex justify-center">
+      <div className="flex flex-col sm:flex-row sm:w-[90%]">
+        <div className="sm:w-[60%]">
           {cartProducts.map((product) => (
             <ShoppingCartProductCard key={product.id} props={product} />
           ))}
         </div>
-        <div className="flex flex-row justify-center items-center self-center py-3 px-3 shadow-xl shadow-slate-300 w-[300px]">
+        <div
+          onClick={() => {
+            navigate("/tienda");
+          }}
+          className="sm:hidden flex flex-row justify-center items-center self-center py-3 px-3 shadow-xl shadow-slate-300 w-[300px]"
+        >
           <img src={ShopCart} alt="" />
           <p>Continuar Comprando</p>
         </div>
-        <div className="mt-5 mx-5">
-          <p className="text-[20px] font-bold border-b border-slate-700 mb-5 pb-2">
+        <div className="mt-5 mx-5 sm:w-[30%] sm:h-[700px] sm:mb-5 sm:border-2 border-slate-800 sm:px-5">
+          <p className="text-[20px] font-bold border-b border-slate-700 my-5 pb-2">
             Resumen del pedido
           </p>
           <div className="flex flex-row w-[100%] justify-between border-b-2 border-slate-700 mb-2 pb-2">
@@ -37,9 +52,9 @@ function ShoppingCartBody() {
               <p>Impuestos</p>
             </div>
             <div className="text-[18px]">
-              <p>1798.00</p>
-              <p>100.00</p>
-              <p>284.79</p>
+              <p>{subTotal}</p>
+              <p>{envio}</p>
+              <p>{impuestos.toFixed(2)}</p>
             </div>
           </div>
           <div className="flex flex-row w-[100%] justify-between text-[20px] font-bold">
@@ -47,7 +62,7 @@ function ShoppingCartBody() {
               <p>Total</p>
             </div>
             <div>
-              <p className="font-normal">2182.79</p>
+              <p className="font-normal">{total.toFixed(2)}</p>
             </div>
           </div>
           <div className="flex mt-5 mb-3 justify-center">
@@ -55,26 +70,29 @@ function ShoppingCartBody() {
               Pagar
             </button>
           </div>
-        </div>
 
-        <div className="mx-5">
-          <div className="flex flex-row">
-            <img src={Check} alt=""></img>
-            <p className="ml-5">Incluye 3 meses de garantía</p>
+          <div className="mx-5">
+            <div className="flex flex-row">
+              <img src={Check} alt=""></img>
+              <p className="ml-5">Incluye 3 meses de garantía</p>
+            </div>
+            <div className="flex flex-row">
+              <img src={Check} alt=""></img>
+              <p className="ml-5">Devoluciones fáciles</p>
+            </div>
+            <div className="flex flex-row">
+              <img src={Check} alt=""></img>
+              <p className="ml-5">Consulta gratuita con expertos</p>
+            </div>
+
+            <div className="mt-5 flex flex-col justify-center items-center">
+              <p>Aceptamos Tarjetas de crédito</p>
+              <img className="w-[300px]" src={CreditCards} alt="" />
+            </div>
           </div>
-          <div className="flex flex-row">
-            <img src={Check} alt=""></img>
-            <p className="ml-5">Devoluciones fáciles</p>
-          </div>
-          <div className="flex flex-row">
-            <img src={Check} alt=""></img>
-            <p className="ml-5">Consulta gratuita con expertos</p>
-          </div>
+          
         </div>
-        <div className="mt-5 flex flex-col justify-center items-center">
-            <p>Aceptamos Tarjetas de crédito</p>
-            <img className="w-[300px]" src={CreditCards} alt="" />
-        </div>
+        
       </div>
     </section>
   );
